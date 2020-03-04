fe652d734c9f3c61a4bb7db442d92147ad2cdace
Rajesh <rajesh.kadam92@gmail.com>
2020-03-04 15:29:14 +0530

    Added Class C

---
+++ Demo/src/Object/C.java
@@ -0,0 +1,16 @@
+package Object;
+
+public class C {
+
+	public void m1()
+	{
+		System.out.println("Hi Git.....");
+	}
+	public static void main(String[] args) {
+		
+		C c = new C();
+		c.m1();
+		
+	}
+	
+}
